724f08813876d1b52f096bf64b821cd6ef5f3fc5
bryandmoreira <bryan.d.moreira@aluno.senai.br>
2026-02-02 17:09:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-function enviarPedido() {
-    if (itemAtual) {
-        // Substitua o 55... pelo seu número
-        let fone = "5541999999999"; 
-        let mensagem = encodeURIComponent(`Olá STARFIC, gostaria de um orçamento para: ${itemAtual}`);
-        
-        window.open(`https://wa.me/${fone}?text=${mensagem}`, '_blank');
-    } else {
-        alert("Selecione um item primeiro!");
-    }
+function enviar(){
+    const nome = document.getElementById('nome').value 
+    const assunto = document.getElementById('assunto').value
+    const mensagem = `Olá STARFIC, gostaria de um orçamento para\n\nNome: ${nome} \nAssunto: ${assunto}`
+    const msg = encodeURIComponent(mensagem)
+    window.open(`https://wa.me/?text=${msg}`)
 }
+
+// Slider com fade real
+const bgDivs = document.querySelectorAll('#s4 .bg');
+
+// Coloque aqui as imagens que você vai usar
+const imagensFundo = [
+    './Images/fi1.jpg',
+    './Images/fi2.png',
+    './Images/fi3.webp'
+];
+
+// Inicializa cada camada com uma imagem
+bgDivs.forEach((div, i) => {
+    div.style.backgroundImage = `url('${imagensFundo[i]}')`;
+});
+
+// Começa com a primeira imagem ativa
+let indice = 0;
+bgDivs[indice].classList.add('active');
+
+// Troca de imagem a cada 5 segundos
+setInterval(() => {
+    bgDivs.forEach(div => div.classList.remove('active'));
+    indice = (indice + 1) % bgDivs.length;
+    bgDivs[indice].classList.add('active');
+}, 5000);
