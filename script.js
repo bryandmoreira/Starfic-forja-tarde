48b1dc97ce25e64428ebb2e906fee3f9f61ef115
bryandmoreira <bryan.d.moreira@aluno.senai.br>
2026-02-04 15:52:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 function enviar(){
     const nome = document.getElementById('nome').value 
     const assunto = document.getElementById('assunto').value
-    const mensagem = `Olá STARFIC, gostaria de um orçamento para\n\nNome: ${nome} \nAssunto: ${assunto}`
+    const codigo = parseFloat(document.getElementById('codigo').value);
+    const mensagem = `Olá STARFIC, gostaria de um orçamento para\n\nNome: ${nome} \n\nCores especificas: ${assunto} \nCódigo: ${codigo}   `
     const msg = encodeURIComponent(mensagem)
-    window.open(`https://wa.me/?text=${msg}`)
+    window.open(`https://wa.me/5541991216822/?text=${msg}`)
 }
 
-// Slider com fade real
+
 const bgDivs = document.querySelectorAll('#s4 .bg');
 
-// Coloque aqui as imagens que você vai usar
+
 const imagensFundo = [
     './Images/fi1.jpg',
     './Images/fi2.png',
     './Images/fi3.webp'
 ];
 
-// Inicializa cada camada com uma imagem
+
 bgDivs.forEach((div, i) => {
     div.style.backgroundImage = `url('${imagensFundo[i]}')`;
 });
 
-// Começa com a primeira imagem ativa
+
 let indice = 0;
 bgDivs[indice].classList.add('active');
 
-// Troca de imagem a cada 5 segundos
+
 setInterval(() => {
     bgDivs.forEach(div => div.classList.remove('active'));
     indice = (indice + 1) % bgDivs.length;
